68bbf67accca9258c04870836f270cd0dd8825c9
mao14sufi <167078726+mao14sufi@users.noreply.github.com>
2024-05-31 18:45:59 +0300

    Add files via upload

---
+++ lab2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace lab2
+{
+    public struct Distance
+    {
+       public int foot;
+       public double inch;
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Distance length1;
+            Distance length2;
+            Distance z;
+            Console.Write("Enter foot1 ");
+            length1.foot = int.Parse(Console.ReadLine());
+            Console.Write("Enter inch1 ");
+            length1.inch = Double.Parse(Console.ReadLine());
+            Console.Write("Enter foot2 ");
+            length2.foot = int.Parse(Console.ReadLine());
+            Console.Write("Enter inch2 ");
+            length2.inch = Double.Parse(Console.ReadLine());
+            
+            z.inch = (length1.inch + length2.inch) % 12;
+            z.foot = length1.foot + length2.foot + (int)(length1.inch + length2.inch) / 12;
+
+            
+            
+            Console.WriteLine($"Sum = {z.foot}'-{z.inch}'' ");
+        }
+    }
+}
