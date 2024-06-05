ff0983d63827567c4c1e69bc9ebf231d2b5f5cfe
mao14sufi <167078726+mao14sufi@users.noreply.github.com>
2024-06-05 19:24:26 +0300

    Add files via upload

--- la5/Program.cs
+++ la5/Program.cs
@@ -1,6 +1,23 @@
 ﻿namespace la5
 {
-    public class Triangle
+    public class Testing
+    {
+        public static void Test()
+        {
+            Console.WriteLine("Enter sides of triangle ");
+            
+            Triangle tr1 = new Triangle(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
+
+            tr1.CheckTriangle();
+            Console.WriteLine($"perimetr of triangle = {tr1.Perimetr():0.00}");
+            Console.WriteLine($"area of triangle = {tr1.Square():0.00}");
+            tr1.OutTriangleSide();
+
+
+
+        }
+    }
+    public class Triangle: IComparable<Triangle>
     {
         private double a, b, c;
 
@@ -19,7 +36,14 @@
         public double Square()
         {
             double p = Perimetr() / 2;
-            return Math.Sqrt(p* (p - a) * (p - b) * (p - c));
+
+            if ( double.IsNaN (Math.Sqrt(p * (p - a) * (p - b) * (p - c))))
+                return 0;
+            else           
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            
+             
+            
         }
         public void CheckTriangle()
         {
@@ -34,22 +58,59 @@
         }
         public void OutTriangleSide()
         {
-            Console.WriteLine($"Triangle side a =  {a} , side b = {b}  , side c = {c} ");
+            Console.WriteLine($"Triangle with side a =  {a} , side b = {b}  , side c = {c} ");
         }
 
+        public int CompareTo(Triangle t)
+        {
+            if (this.Square() == t.Square())
+                return 0;
+            else if (this.Square() > t.Square())
+                return 1;
+            else
+                return -1;
+        }
+        
     }       
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter sides of triangle ");
-            Triangle tr1 = new Triangle(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
             
-            tr1.CheckTriangle();
-            Console.WriteLine($"perimetr of triangle = {tr1.Perimetr():0.00}");
-            Console.WriteLine($"square of triangle = {tr1.Square():0.00}");
-            tr1.OutTriangleSide();
+            Triangle[] triangles = new Triangle[3];
+
+            for (int i = 0; i < triangles.Length; i++)
+            {
+                Console.WriteLine($"Enter sides of triangle {i+1} ");
+                triangles[i] = new Triangle(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
+
+            }
+            
+
+
+            Array.Sort(triangles);
+
+            foreach (Triangle tr in triangles)
+            {
+                Console.WriteLine();
+
+                tr.CheckTriangle();
+                tr.OutTriangleSide();
+
+                Console.WriteLine($"area = {tr.Square():0.00}");
+            }
+
+
+            //Testing.Test();
+
+            //Console.WriteLine("Enter sides of triangle ");
+            //Triangle tr1 = new Triangle(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
+
+            //tr1.CheckTriangle();
+            //Console.WriteLine($"perimetr of triangle = {tr1.Perimetr():0.00}");
+            //Console.WriteLine($"square of triangle = {tr1.Square():0.00}");
+            //tr1.OutTriangleSide();
 
         }
     }
